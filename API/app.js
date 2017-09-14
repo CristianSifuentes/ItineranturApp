67727928312bc51575b2882ec09d37f480f936f4
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2017-09-14 12:54:33 -0500

    20170914 CSC: Pimero commit

---
+++ API/app.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env._port || 3977;
+
+mongoose.connect('mongodb://localhost:/DbItinerantur', (err, res) => {
+    if (err) {
+        throw err;
+    } else {
+       console.log('la conexión a la base de datos está corriendo corractamente..');
+      app.listen(port, function(){
+            console.log('El servidor esta recibiendo peticiones');
+      });
+    }
+});
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ItineranturApp
+Itinerantur es una aplicación desarrollada con MEAN Full Stack, la cual te 
+permite compartir las fotografías de tus viajes con otros viajeros.
