65f4a370b9fc5312cb95ed7f49f5d1b2defe166c
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2017-09-14 16:28:15 -0500

    20171409 CSC: Se agregan las dependencias para el uso de flexlayout y angular-material

--- CLIENT/src/app/app.module.ts
+++ CLIENT/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    FlexLayoutModule,
+    MdButtonModule,
+    MdCardModule,
+    MdMenuModule,
+    MdToolbarModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
