17ffe0cdc8e1c737b05ec56ecbf2a10a2b8b4c37
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2017-09-15 13:11:07 -0500

    20170915 CSC: Cambio de estilos

--- CLIENT/src/app/sign-up/sign-up.component.ts
+++ CLIENT/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  public signupForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      nick: ['', Validators.required],
+      email: ['', Validators.required],
+      password : ['', Validators.required]
+    });
   }
 
 }
